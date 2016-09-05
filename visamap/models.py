07fa6a1addd4d5882f1fcac7be1422dc37677bfa
Miguel Brito <miguelandres77@gmail.com>
2016-09-05 11:21:23 -0400

    Created models

--- visamap/models.py
+++ visamap/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Demonym(models.Model):
+    description = models.CharField(max_length=50)
+    country = models.ForeignKey(Country)
+
+
+class VisaType(models.Model):
+    description = models.TextField(unique=True)
+
+
+class Requirement(models.Model):
+    origin_country = models.ForeignKey(Country)
+    destination_country = models.ForeignKey(Country)
+    visa_type = models.ForeignKey(VisaType)
+    observations = models.TextField(null=True)
+    period = models.DurationField(null=True)
--- visarequirements/settings.py
+++ visarequirements/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'visarequirements.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['VISAREQUIREMENTS_DB_NAME'],
+        'USER': os.environ['VISAREQUIREMENTS_DB_USER'],
+        'PASSWORD': os.environ['VISAREQUIREMENTS_DB_PASSWORD'],
+        'HOST': os.environ['VISAREQUIREMENTS_DB_HOST'],
+        'PORT': os.environ['VISAREQUIREMENTS_DB_PORT'],
     }
 }
 
